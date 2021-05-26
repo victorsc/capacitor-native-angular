e6b10ba88c93542c2ab2d02f44661bd5a2472a3d
Victor Scaiceanu <victor.scaiceanu@amadeus.com>
2021-05-26 12:01:32 +0200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.victorsc.capacitor',
+  appName: 'capacitor native angular',
+  webDir: 'dist/capacitor-native-angular',
+  bundledWebRuntime: false
+};
+
+export default config;
